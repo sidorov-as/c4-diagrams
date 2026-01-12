3eb539ea5394477e80cfcad657e4e06df563799c
sidorov-as <ereon.dev@gmail.com>
2026-01-12 15:39:56 +0300

    refactor: add PlantUML renderer imports

--- c4/renderers/plantuml/__init__.py
+++ c4/renderers/plantuml/__init__.py
@@ -1,7 +1,15 @@
+from c4.renderers.plantuml.backends import (
+    DiagramFormat,
+    LocalPlantUMLBackend,
+    RemotePlantUMLBackend,
+)
 from c4.renderers.plantuml.layout_options import LayoutOptions
 from c4.renderers.plantuml.renderer import PlantUMLRenderer
 
 __all__ = (
+    "DiagramFormat",
     "LayoutOptions",
+    "LocalPlantUMLBackend",
     "PlantUMLRenderer",
+    "RemotePlantUMLBackend",
 )
